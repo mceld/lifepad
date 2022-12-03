6259a2c9a908024d09663bbe62538aabbedc8708
mceld <lwm2120@rit.edu>
2022-12-03 15:55:05 -0500

    version of gen coords from hw repo

--- gen_coords.py
+++ gen_coords.py
@@ -1,7 +1,30 @@
 import sys
 
+#
+# 1 -> name
+# 2 -> description
+# 3 -> image
+# 4 -> coords file
+#
+
 def main():
-	
+    if len(sys.argv) != 5:
+        print("wrong args")
+        return
+
+    name = sys.argv[1]
+    description = sys.argv[2]
+    image = sys.argv[3]
+    coords_file = sys.argv[4]
+
+    coords = []
+
+    with open(coords_file) as f:
+        for line in f:
+            split = line.split()
+            coords.append([split[0], split[1]])
+
+    
 
 if __name__ == "__main__":
-	main()
+    main()
